02323bfb0712f565380b536875895805e77d9bfd
Jose <jose@jose-P5K-PRO.(none)>
2013-05-20 10:42:11 +0200

    quinto commit

--- principal/views.py
+++ principal/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from util import decoradores
 
 def lista_zapatos(request):
-    zapatos = Zapato.objects.all().order_by('tipo')
+    zapatos = Zapato.objects.all()
     series = Series.objects.all()
     context_instance=RequestContext(request)
     return render_to_response('inicio.html',{'lista':zapatos,'series':series},context_instance)
@@ -65,3 +65,39 @@ def tienda (request):
 	empleados = Empleados.objects.all()
 	context_instance = RequestContext(request)
 	return render_to_response('contacto.html',{'dato':dato,'empleados':empleados},context_instance)
+
+def zapato_marcanino(request, id_marca):
+	dato = get_object_or_404(Marca, pk=id_marca)
+	zapato = Zapato.objects.filter(marca = dato, genero = 1)
+	context_instance = RequestContext(request)
+	return render_to_response('producto_marcanino.html',{'dato':dato, 'zapato':zapato},context_instance)
+
+def zapato_marcanina(request, id_marca):
+	dato = get_object_or_404(Marca, pk=id_marca)
+	zapato = Zapato.objects.filter(marca = dato, genero = 2)
+	context_instance = RequestContext(request)
+	return render_to_response('producto_marcanina.html',{'dato':dato, 'zapato':zapato},context_instance)
+
+def zapato_marcauni(request, id_marca):
+	dato = get_object_or_404(Marca, pk=id_marca)
+	zapato = Zapato.objects.filter(marca = dato, genero = 3)
+	context_instance = RequestContext(request)
+	return render_to_response('producto_uni.html',{'dato':dato, 'zapato':zapato},context_instance)
+
+def zapato_tiponino(request, id_tipo):
+	dato = get_object_or_404(Tipo, pk=id_tipo)
+	zapato = Zapato.objects.filter(tipo = dato, genero = 1)
+	context_instance = RequestContext(request)
+	return render_to_response('productos_tiponino.html',{'dato':dato, 'zapato':zapato},context_instance)
+
+def zapato_tiponina(request, id_tipo):
+	dato = get_object_or_404(Tipo, pk=id_tipo)
+	zapato = Zapato.objects.filter(tipo = dato, genero = 2)
+	context_instance = RequestContext(request)
+	return render_to_response('productos_tiponina.html',{'dato':dato, 'zapato':zapato},context_instance)
+
+def zapato_tipouni(request, id_tipo):
+	dato = get_object_or_404(Tipo, pk=id_tipo)
+	zapato = Zapato.objects.filter(tipo = dato, genero = 3)
+	context_instance = RequestContext(request)
+	return render_to_response('productos_tipouni.html',{'dato':dato, 'zapato':zapato},context_instance)
