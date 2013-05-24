328224a34c7dda87e4d3b3228a4ef894f95e7108
jose <zeccus@gmail.com>
2013-05-24 11:22:52 +0200

    decimotercer commit

--- zapateria/settings.py
+++ zapateria/settings.py
@@ -160,3 +160,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'calzadosgarabatos33@gmail.com'
 EMAIL_HOST_PASSWORD = 'garabatosbaratos'
 EMAIL_PORT = 587
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
