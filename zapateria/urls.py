e8cce4f0a316359d6a878044c44afcbffb356ac9
jose <zeccus@gmail.com>
2013-05-27 09:46:20 +0200

    13 commit

--- zapateria/urls.py
+++ zapateria/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$','principal.views.lista_zapatos'),
+    url(r'^$','principal.views.tipo'),
     url(r'^proovedores/$','principal.views.lista_proovedores'),
     url(r'^producto/(?P<id_zapato>\d+)$','principal.views.zapato_especifico',name = 'principal_productos'),
     url(r'^ninos/$','principal.views.zapato_nino', name = 'ninos'),
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^unisex/$','principal.views.zapato_unisex', name = 'uni'),
     url(r'^marcas/$','principal.views.marca'),
     url(r'^marca_producto/(?P<id_marca>\d+)$','principal.views.zapato_marca',name = 'principal_marcas'),
-    url(r'^tipo/$','principal.views.tipo'),
+    url(r'^lista/$','principal.views.lista_zapatos'),
     url(r'^tipo_zapato/(?P<id_tipo>\d+)$','principal.views.tipo_zapato',name = 'principal_tipos'),
     url(r'^marcas/nino/(?P<id_marca>\d+)$','principal.views.zapato_marcanino',name = 'ninomarca'),
     url(r'^marcas/nina/(?P<id_marca>\d+)$','principal.views.zapato_marcanina',name = 'ninamarca'),
